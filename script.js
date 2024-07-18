4644464eb12428c61bd42efded2d1c3f2de2d00d
sa20122424 <saloxiddin201224@gmail.com>
2024-07-18 18:29:59 +0500

    salom

---
+++ script.js
@@ -0,0 +1,25 @@
+// const eshak = +prompt(100)
+// const eshak1 = +prompt(24)
+
+// if (eshak => eshak1) {
+//     console.log(`eshak miya true ${eshak}`);
+// }else {
+//     console.log(`eshak miya folse ${eshak1}`);
+
+
+
+//  }
+
+
+
+
+
+
+
+const random = (Math.round(Math.random() * 100))
+alert(`Рандомное число: ${random}`)
+if (random % 2 == 0 ) {
+    alert(`${random} чотное `)
+} else {
+    alert(`${random} нечотное `)
+}
